ba15b8ceaeeffa15b9cd968793caaa2d0470e73f
Alex <opetrovskyi@mggpaero.com>
2021-11-17 13:45:15 +0100

    marvel

---
+++ marvel_starter/src/pages/404.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ErrorMessage from "../components/errorMessage/ErrorMessage";
+
+const NoMatch = () => {
+    return (
+        <div>
+            <ErrorMessage/>
+            NoMatch....
+        </div>
+    );
+};
+
+export default NoMatch;
