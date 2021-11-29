3d9ca29ff3e455d402b054d397d36d15eef502cf
Alex <opetrovskyi@mggpaero.com>
2021-11-29 09:48:56 +0100

    139-start-form React Context

--- tests/src/App.js
+++ tests/src/App.js
@@ -1,61 +1,50 @@
-import {useState} from 'react';
+import {Component, useState} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
 
-function useInputWithValidate(initialValue) {
-    const [value, setValue] = useState(initialValue)
-
-    const onChange = event => {
-        setValue(event.target.value)
-    }
-
-    const validateInp = () => {
-        return value.search(/\d/) >= 0
-    }
-
-    return {value: value, onChange: onChange,validateInp}
-}
-
-const Form = () => {
-    const input = useInputWithValidate('')
-    const textArea =useInputWithValidate('')
-
-    const form_control = 'form-control'
-    const color = input.validateInp() ? 'text-danger' : null
+const Form = (props) => {
+    console.log('render')
 
     return (
         <Container>
             <form className="w-50 border mt-5 p-3 m-auto">
                 <div className="mb-3">
-                    <input value={`${input.value} / ${textArea.value}`} type="text" className="form-control" readOnly/>
                     <label htmlFor="exampleFormControlInput1" className="form-label mt-3">Email address</label>
-                    <input
-                        value={input.value}
-                        onChange={input.onChange}
-                        type="email"
-                        className={`${form_control && color} `}
-
-                        placeholder="name@example.com"/>
+                    <InputComponent mail={props.mail}/>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-                    <textarea
-                        value={textArea.value}
-                        onChange={textArea.onChange}
-                        className="form-control"
-                        id="exampleFormControlTextarea1"
-                        rows="3"
-                    />
-
+                    <textarea value={props.text} className="form-control" id="exampleFormControlTextarea1" rows="3"/>
                 </div>
             </form>
         </Container>
     )
 }
+class InputComponent extends Component {
+    render() {
+        return(
+            <input value={this.props.mail} type="email" className='form-control' id="exampleFormControlInput1" placeholder="name@example.com"/>
+        )
+    }
+}
 
 function App() {
+    const [data, setData] = useState({
+        mail: "name@example.com",
+        text: 'some text'
+    });
+
     return (
-        <Form/>
+        <>
+            <Form mail={data.mail} text={data.text}/>
+            <button
+                onClick={() => setData({
+                    mail: "second@example.com",
+                    text: 'another text'
+                })}>
+                Click me
+            </button>
+        </>
     );
 }
 
